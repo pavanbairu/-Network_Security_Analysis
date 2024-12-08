76698580983d2436a24293b929385e1a493c4933
pavanbairu <pavan.bairu1@gmail.com>
2024-12-08 20:40:45 +0530

    implemented data_ingestion

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -38,7 +38,15 @@ class TrainingPipeline():
 
     def start_data_ingestion(self):
         try:
-            pass
+            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Starting data ingestion")
+
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+            
+            return data_ingestion_artifact
+            
         except Exception as e:
             raise NetworkSecurityException(e,sys)
 
